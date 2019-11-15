7554bd4fe821acbef3568ed4ca38aa5281897312
Milo Calvo-Platero <macp92@gmail.com>
2019-11-15 16:20:45 -0500

    first commit

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,17 +1,72 @@
-import React from 'react';
-import { IonHeader, IonToolbar, IonPage, IonTitle, IonContent } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg, IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import React, { Component } from 'react';
+import { Plugins, CameraResultType } from '@capacitor/core';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import Tesseract from 'tesseract.js';
 
-const Tab3Page: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab Three</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent />
-    </IonPage>
-  );
+
+
+
+const { Camera } = Plugins;
+const INITIAL_STATE = {
+  photo: '',
+  text: '',
 };
+export class Tab3Page extends Component {
+  state: any = {};
+  props: any = {};
+  constructor(props: any) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+    defineCustomElements(window);
+  }
+  async takePicture() {
+    const image = await Camera.getPhoto({
+    quality: 90,
+    allowEditing: false,
+    resultType: CameraResultType.Uri
+    });
+    let outputText
+    await Tesseract.recognize(
+      `${image.webPath}`,
+      'eng',
+      { logger: m => console.log(m) }
+    ).then(({ data: { text } }) => {
+      console.log(text)
+      outputText = text;
+    })
 
+    var imageUrl = image.webPath;
+    // Can be set to the src of an image now
+    this.setState({
+    photo: imageUrl,
+    text: outputText
+    })
+
+    }
+
+  render() {
+    const { photo, text } = this.state;
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Camera Page</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonImg style={{ 'border': '1px solid black', 'minHeight': '100px' }} src={photo} ></IonImg>
+          <div>
+              Text should appear here: {text}
+          </div>
+          <IonFab color="primary" vertical="bottom" horizontal="center" slot="fixed">
+            <IonFabButton color="primary" onClick={() => this.takePicture()}>
+              <IonIcon name="add" />
+            </IonFabButton>
+          </IonFab>
+        </IonContent>
+      </IonPage >
+    );
+  };
+}
 export default Tab3Page;
